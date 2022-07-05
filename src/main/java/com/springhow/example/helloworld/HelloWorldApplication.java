5e4dccea026c099b42d4886b9cddabb356b1c250
Aunk237 <107750997+Aunk237@users.noreply.github.com>
2022-07-05 18:51:29 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "this is final time you gusy are doing pipeline so wait !!";
+        return "this is twelth time you gusy are doing pipeline so wait !!";
     }
 
 }
